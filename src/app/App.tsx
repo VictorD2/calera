9e6b1bb7f69425736cce40d3a24358461dd05b0c
JFBJ17 <razorij20@gmail.com>
2021-06-30 14:48:29 -0500

    Modal menú agregado

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //Pages
 import Home from '../pages/Home';
+import Footer from '../partials/Footer';
 import Tienda from '../pages/Tienda';
+import Header from '../partials/Header';
 
 function App() {
 
@@ -15,10 +17,12 @@ function App() {
   }, []);
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/Tienda" component={Tienda} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/pages/Tienda.tsx
+++ src/pages/Tienda.tsx
@@ -1,21 +1,14 @@
 import React from 'react'
 
-
-//Partials
-import Header from '../partials/Header';
-import Footer from '../partials/Footer';
-
 //Componentes
 import ListaProductos from '../components/Productos/ListaProductos';
 
 const Tienda = () => {
     return (
         <>
-            <Header />
             <main>
                 <ListaProductos />
             </main>
-            <Footer />
         </>
     )
 }
